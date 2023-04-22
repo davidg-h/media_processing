c8ee148580808aa9d2509134667a8d5b51162a99
DavidNguyen <davidGit@gmx.de>
2023-04-22 18:36:51 +0200

    updated e03

--- e03/README.md
+++ e03/README.md
@@ -1,2 +1,3 @@
 # Using OpenCV to work in pictures
-task: read in an image display it and get the rgb values on the mouse-pointer position to be displayed on a mouse click
+## Description 
+Read in an image display it and get the rgb values on the mouse-pointer position to be displayed on a mouse click
--- e03/main.py
+++ e03/main.py
@@ -2,51 +2,60 @@ import cv2
 import numpy as np
 
 # read in an image and display it in a window
-image = cv2.imread("rose.jpg")
+image = cv2.imread("forrest.jpg")
 
 # get the rgb value of a pixel on click 
 def mouseRGB(event,x,y,flags,param):
     global image
     if event == cv2.EVENT_LBUTTONDOWN: #checks mouse left button down condition
         # Clear previous text from the image
-        image = cv2.imread("rose.jpg")
+        image = cv2.imread("forrest.jpg")
         
         colorsB = image[y,x,0]
         colorsG = image[y,x,1]
         colorsR = image[y,x,2]
+        # Clear image when the text is clicked
+        if colorsB == colorsG == colorsR == 255:
+            image = cv2.imread("forrest.jpg")
+            
         print("Red: ",colorsR)
         print("Green: ",colorsG)
         print("Blue: ",colorsB)
         print("Coordinates of pixel: X: ",x,"Y: ",y)
+        
         # Convert RGB to BGR
         bgr = (colorsB, colorsG, colorsR)
+        
         # Convert BGR to HSV
         hsv = cv2.cvtColor(np.uint8([[bgr]]), cv2.COLOR_BGR2HSV)[0][0]
         print("Hue:", hsv[0])
         print("Saturation:", hsv[1])
         print("Value:", hsv[2])
+        
        # Display the RGB value as text on the image window
         font = cv2.FONT_HERSHEY_SIMPLEX
-        text = f"RGB:{colorsR},{colorsG},{colorsB}\nHSV:{hsv[0]},{hsv[1]},{hsv[2]}"
-        font_scale = 0.5
-        font_thickness = 1
+        text_rgb = f"RGB:{colorsR},{colorsG},{colorsB}"
+        text_hsv = f"HSV:{hsv[0]},{hsv[1]},{hsv[2]}"
+        font_scale = 0.001*image.shape[0]
+        font_thickness = 5
         
         # Get the size of the text
-        (text_width, text_height), _ = cv2.getTextSize(text, font, font_scale, font_thickness)
-        
-        # Calculate the maximum font size that can fit within the image
-        max_font_scale = min(image.shape[0] / text_height, image.shape[1] / text_width)
-        font_scale *= max_font_scale
-        
-        # Recalculate the size of the text with the new font size
-        (text_width, text_height), _ = cv2.getTextSize(text, font, font_scale, font_thickness)
-        
-        # Calculate the position of the text so that it's centered on the clicked pixel
+        (text_width, text_height), _ = cv2.getTextSize(text_rgb, font, font_scale, font_thickness)
+        text_rgb_height = text_height
+        # Calculate the position of the text so that it's aligned to the top of the clicked pixel
         x_pos = max(0, x - text_width // 2)
-        y_pos = max(text_height, y - 10)
+        y_pos = max(text_height, y - 10 - text_height)
         
-        # Display the text on the image
-        cv2.putText(image, text, (x_pos, y_pos), font, font_scale, (255, 255, 255), font_thickness)
+        cv2.putText(image, text_rgb, (x_pos, y_pos), font, font_scale, (255, 255, 255), font_thickness)
+        
+        # Get the size of the text
+        (text_width, text_height), _ = cv2.getTextSize(text_hsv, font, font_scale, font_thickness)
+        
+        # Calculate the position of the text so that it's aligned to the top of the clicked pixel
+        x_pos = max(0, x - text_width // 2)
+        y_pos = max(text_height, y - 10 - text_height)+text_rgb_height+5
+        
+        cv2.putText(image, text_hsv, (x_pos, y_pos), font, font_scale, (255, 255, 255), font_thickness)
    
 # Read an image, a window and bind the function to window 
 cv2.namedWindow('mouseRGB')
